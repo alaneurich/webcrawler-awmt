4d1ae4506c0af3e655514079f1dfda319e75b282
Tim Elbert <tim.elbert@iem.thm.de>
2021-01-19 17:00:21 +0100

    Simple Crawler: Final Touches

--- crawlers/nabu_simple_crawler/index.js
+++ crawlers/nabu_simple_crawler/index.js
@@ -12,17 +12,22 @@ const crawler = new simplecrawler('https://www.nabu.de/news/18446.html')
 
 crawler.maxDepth = 2
 
-const domainPattern = /\/news\/\d{4}\/\d{2}\//g
+const domainPattern = /\/news\/(\d{4})\/(\d{2})\//
 
 crawler.addFetchCondition((queueItem, referrerQueueItem, callback) => {
   callback(null, domainPattern.test(queueItem.url))
 })
 
 crawler.on('fetchcomplete', (queueItem, responseBuffer, response) => {
+  if(!domainPattern.test(queueItem.url)) return
   if(!!db.get('pages').find({ url: queueItem.url }).value()) return
 
+  let [x, year, month] = domainPattern.exec(queueItem.url) || []
+
   let page = {
-    url: queueItem.url
+    url: queueItem.url,
+    year: parseInt(year),
+    month: parseInt(month),
   }
 
   if(response.headers['content-type'].includes('text/html')){
@@ -36,14 +41,23 @@ crawler.on('fetchcomplete', (queueItem, responseBuffer, response) => {
     for(let i of Array(6).keys()){
       let heading = document.querySelector(`#readspeaker_content h${i + 1}`)
       if(heading){
+
         heading = heading.textContent.replace(/\s+/g, ' ').trim()
-        if(heading) page.elements[`h${i + 1}`] = heading
+        if(heading){
+          if(!page.elements.headings) page.elements.headings = {}
+          page.elements.headings[`h${i + 1}`] = heading
+        }
       }
     }
 
     let paragraphs = document.querySelectorAll('#readspeaker_content p')
     if(paragraphs){
       page.elements.paragraphs = Array.from(paragraphs).map(e => e.textContent.replace(/\s+/g, ' ').replace('Mehr →', '').trim()).filter(e => e.length)
+    }
+
+    let images = document.querySelectorAll('#readspeaker_content img')
+    if(images){
+      page.elements.images = Array.from(images).map(e => e.src)
     }
   }
 
