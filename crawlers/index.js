020892029b347848e8708a46bb404c009d2838bd
Tim Elbert <tim.elbert@iem.thm.de>
2021-01-19 15:53:45 +0100

    Strukturänderung

--- crawlers/index.js
+++ crawlers/index.js
@@ -1,9 +1,9 @@
 const handelsblatt = require('./handelsblatt')
 const nabuCrawler = require('./nabu_crawler')
-const nabuSimpleCrawler = require('./nabu_crawler')
+const nabuSimpleCrawler = require('./nabu_simple_crawler')
 
 module.exports = {
-    nabuCrawler: nabuCrawler.main,
-    nabuSimpleCrawler: nabuSimpleCrawler.main,
-    handelsblatt: handelsblatt.main,
+    nabuCrawler,
+    nabuSimpleCrawler,
+    handelsblatt,
 }
--- crawlers/nabu_simple_crawler.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    main: () => {
-        console.log('nabu');
-    }
-}
