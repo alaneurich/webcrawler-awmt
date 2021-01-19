4ca1303088e397acafe2df9766f1fd0f7b2cde05
Tim Elbert <tim.elbert@iem.thm.de>
2021-01-19 17:05:33 +0100

    Nabu Crawler: Refaktorisiert

--- crawlers/nabu_crawler/index.js
+++ crawlers/nabu_crawler/index.js
@@ -1,37 +1,38 @@
-var Crawler = require("crawler")
+const Crawler = require("crawler")
+
+const c = new Crawler({
+    maxConnections: 10,
+    callback: callbackX
+})
+
+function callbackX(error, res, done) {
+    if (error) {
+        console.log(error)
+    } else {
+        let $ = res.$
+        $(".related-content > div > a").each( function() {
+            c.queue([{
+                uri: $(this).attr("href"),
+                jQuery: true,
+                callback: callbackY
+            }])
+        })
+    }
+    done()
+}
+
+function callbackY(error, res, done) {
+    if (error) {
+        console.log(error)
+    } else {
+        var $ = res.$
+        $("#readspeaker_content :matches(h1, h2, h3, h4, h5, h6, p)").each(function() {
+            console.log($(this).text().trim())
+        })
+    }
+    done()
+}
 
 module.exports = () => {
-    var c = new Crawler({
-        maxConnections: 10,
-
-        callback: function (error, res, done) {
-            if (error) {
-                console.log(error)
-            } else {
-                var $ = res.$
-                $(".related-content > div > a").each( function() {
-                    const url = $(this).attr("href")
-                    c.queue([{
-                        uri: url,
-                        jQuery: true,
-
-                        callback: function (error, res, done) {
-                            if (error) {
-                                console.log(error)
-                            } else {
-                                var $ = res.$
-                                $("#readspeaker_content :matches(h1, h2, h3, h4, h5, h6, p)").each(function() {
-                                    console.log($(this).text().trim())
-                                })
-                            }
-                            done();
-                        }
-                    }])
-                })
-            }
-            done();
-        }
-    })
-
     c.queue("https://www.nabu.de/news/18446.html")
 }
